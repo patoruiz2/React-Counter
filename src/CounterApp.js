8209e63b9bfaa7756e4a6a32f4e1ea370194ec49
Patricio Ruiz <patoruiz998@gmail.com>
2021-03-30 16:52:17 -0300

    react-counter

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -28,7 +28,7 @@ const CounterApp = ({value = 10})=>{
             <h1>CounterApp</h1>
             <h2>{counter}</h2>
 
-            <button onClick={handleAdd/}>+1</button>
+            <button onClick={handleAdd}>+1</button>
 
             <button onClick={handleReset}>Reset</button>
             
